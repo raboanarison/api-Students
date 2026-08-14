1aaafedf4538d31279567fe1192e1667df99443f
Amin <raboanarisonamin@gmail.com>
2026-08-14 07:13:55 +0300

    Use environment variables for database configuration

--- src/configuration/db.ts
+++ src/configuration/db.ts
@@ -1,9 +1,12 @@
 import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const pool = new Pool({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: "shadow",
-    database: "amin"
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
